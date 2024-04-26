31ed0ae841a36170e13ec5ec5bf2232f9c0f37b8
sudoken <44742486+adieumonks@users.noreply.github.com>
2024-04-27 00:48:11 +0900

    add evaluator to repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/adieumonks/monkey/evaluator"
 	"github.com/adieumonks/monkey/lexer"
 	"github.com/adieumonks/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
